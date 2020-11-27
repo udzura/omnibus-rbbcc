ff51e36ea0fc44d62fbff613c02e7a27e6919175
Uchio Kondo <udzura@udzura.jp>
2020-11-27 22:10:21 +0900

    Add the workflow

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 eval "$(rbenv init -)"
+
+git config --global user.email "udzura@udzura.jp"
+git config --global user.name "Uchio Kondo"
+bundle config set path '/var/bundle'
+bundle install
+bundle binstub --force omnibus
+
+# So bad hack!!
+sed -i -e 's/libffi/ffi/' "$(find /tmp/bundle -name 'software' | xargs -I{} find  {} -name 'ruby.rb')"
+
 ./bin/omnibus build "$@"
