73452328985209adc1f76a3dfabb763409b9661c
Uchio Kondo <udzura@udzura.jp>
2020-11-28 02:20:20 +0900

    Re-gen embedded bundle cmd

--- config/software/rbbcc.rb
+++ config/software/rbbcc.rb
@@ -17,6 +17,7 @@ build do
   bundle "install --without plugin_dev", env: env
   bundle "exec rake install", env: env
   bundle "binstubs appbundler --path #{install_dir}/embedded/bin", env: env
+  bundle "binstubs bundler --path #{install_dir}/embedded/bin", env: env
 
   # gem "build #{gemspec_name}", env: env
 
